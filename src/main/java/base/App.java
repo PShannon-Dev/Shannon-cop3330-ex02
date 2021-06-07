4085ea870f27434833f4c4b3788cabdc9155338c
paulshannon-coding <mikey4295@hotmail.com>
2021-06-06 20:51:29 -0400

    fix pom

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,3 +1,7 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Paul Shannon
+ */
 package base;//base package to deliver the first part of the exercise
 
 import java.util.Scanner;//package for scanner used latter
